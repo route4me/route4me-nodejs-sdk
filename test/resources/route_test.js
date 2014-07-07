9de162ee116c14146ac0decfcd54e5f32f1e550e
Skrynkovskyy Igor <skrynkovskyy@gmail.com>
2014-07-08 02:55:37 +0300

    use new route

--- test/resources/route_test.js
+++ test/resources/route_test.js
@@ -2,7 +2,21 @@ var expect = require('chai').expect
   , utils = require('../utils')
   , route4me = utils.route4me;
 
+var r = null;
+
 describe('Route API', function() {
+  before(function(done) {
+    var params = { disable_optimization: true };
+
+    utils.createRoute(params, function(err, optimization) {
+      var problem_id = optimization.optimization_problem_id;
+      route4me.OptimizationProblem.get(problem_id, function(err, problem) {
+        r = problem['routes'][0];
+        done();
+      });
+    });
+  });
+
   it("should return right methods", function(done) {
     expect(route4me).to.contain.keys('Route');
     expect(route4me.Route).to.contain.keys(['get', 'update', 'delete']);
@@ -11,12 +25,10 @@ describe('Route API', function() {
   });
 
   it("should return route info by route_id", function(done) {
-    var route_id = 'AEB5EE5ED65BCFB058E5C89D1906E924';
-
-    route4me.Route.get(route_id, function (err, route) {
+    route4me.Route.get(r.route_id, function (err, route) {
       expect(err).to.be.null;
       expect(route).to.contain.keys(['route_id', 'member_id']);
-      expect(route.route_id).to.equal(route_id);
+      expect(route.route_id).to.equal(r.route_id);
       expect(route.member_id).to.equal('1');
 
       done();
@@ -25,7 +37,7 @@ describe('Route API', function() {
 
   it("should return routes info by list of route_id", function(done) {
     var route_ids = [
-      'AEB5EE5ED65BCFB058E5C89D1906E924'
+      r.route_id
     , '6D756FDB0830F26731EE05AA175CBB62'
     ];
 
@@ -49,13 +61,12 @@ describe('Route API', function() {
   });
 
   it("should update route_name", function (done) {
-    var route_id = 'AEB5EE5ED65BCFB058E5C89D1906E924';
     var route_name = 'Route updated on ' + new Date();
     var params = {
       parameters: { route_name: route_name }
     };
 
-    route4me.Route.update(route_id, params, function (err, route) {
+    route4me.Route.update(r.route_id, params, function (err, route) {
       expect(err).to.be.null;
       expect(route.parameters.route_name).to.equal(route_name);
 
@@ -64,7 +75,6 @@ describe('Route API', function() {
   });
 
   it("should add address to route", function (done) {
-    var route_id = 'AEB5EE5ED65BCFB058E5C89D1906E924';
     var params = {
       addresses: [
         {
@@ -75,18 +85,16 @@ describe('Route API', function() {
       ]
     };
 
-    route4me.Route.update(route_id, params, function (err, route) {
+    route4me.Route.update(r.route_id, params, function (err, route) {
       expect(err).to.be.null;
-      expect(route.route_id).to.equal(route_id);
+      expect(route.route_id).to.equal(r.route_id);
 
       done();
     });
   });
 
   it("should remove route", function(done) {
-    var route_id = 'AEB5EE5ED65BCFB058E5C89D1906E924';
-
-    route4me.Route.delete(route_id, function (err, deleted) {
+    route4me.Route.delete(r.route_id, function (err, deleted) {
       expect(err).to.be.null;
       expect(deleted).to.equal(true);
 
