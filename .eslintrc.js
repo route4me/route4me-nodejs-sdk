d5f949616966705ba130697ea5b3775199471fd7
maxkoryukov <maxkoryukov@yandex.ru>
2017-01-20 13:21:08 +0500

    Customized eslint.config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,16 @@ exports = module.exports = {
 		"airbnb"
 	],
 	"rules": {
-		"dot-notation": "off"
+		"dot-notation": "off",
+
+		// code style
+		"indent": ["warn", "tab"],
+		"no-tabs": "off",
+
+		// double quotes (JSON love ", and not ')
+		// and we like to copypaste time to time
+		"quotes": ["warn", "double"],
+
+		"no-underscore-dangle": ["error", { "allowAfterThis": true }],
 	}
 }
--- examples/reoptimization.js
+++ examples/reoptimization.js
@@ -1,15 +1,17 @@
-var api_key = '11111111111111111111111111111111'
-  , route4me = require('..')(api_key)
-  , addresses = require('./addresses.json');
-  j
+const Route4Me = require("..");
+const addresses = require("./addresses.json");
 
-var problem_id: 'F2FEA85DA7EFCE180CAD70704816347A';
-var params = {
-  parameters: { reoptimize: true }
-, addresses: addresses
-, reoptimize: true
+const api_key = "11111111111111111111111111111111";
+
+const route4me = new Route4Me(api_key);
+
+const problem_id = "F2FEA85DA7EFCE180CAD70704816347A";
+const params = {
+	parameters: { reoptimize: true },
+	addresses,
+	reoptimize: true,
 };
 
-route4me.OptimizationProblem.update(problem_id, params, function(err, problem) {
-  console.log(err, problem);
+route4me.OptimizationProblem.update(problem_id, params, (err, problem) => {
+	console.log(err, problem);
 });
