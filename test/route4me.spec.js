2096111721f63a4404216231882a649b27870e9a
maxkoryukov <maxkoryukov@yandex.ru>
2017-01-21 06:08:34 +0500

    Several improvements: * userAgent * parse options * handle errors

--- test/route4me.spec.js
+++ test/route4me.spec.js
@@ -3,20 +3,17 @@ const Route4Me = require("../lib/route4me")
 const testApiKey = "11111111111111111111111111111111"
 
 describe("route4me.spec", () => {
-
 	it("should run with new", () => {
 		const route4me = new Route4Me(testApiKey)
 
-		expect(route4me).to.be.an('object')
+		expect(route4me).to.be.an("object")
 		expect(route4me).to.be.an.instanceof(Route4Me)
 
-		//expect(route4me.url_base).to.equal("http://route4me.com")
+		// expect(route4me.url_base).to.equal("http://route4me.com")
 	})
 
 	describe("call constructor with wrong (empty) apiKey", () => {
-
-		[null, undefined, false, ''].forEach( apiKey => {
-
+		[null, undefined, false, ""].forEach((apiKey) => {
 			it(`should raise when apiKey=${apiKey}`, () => {
 				const fn = () => new Route4Me(apiKey)
 				expect(fn).to.throw(/apikey is not set/i)
@@ -25,9 +22,9 @@ describe("route4me.spec", () => {
 	})
 
 	describe("methods", () => {
-		it("getOptimizations", done => {
+		it("getOptimizations", (done) => {
 			const route4me = new Route4Me(testApiKey)
-			route4me.getOptimizations(1,1,1, done)
+			route4me.getOptimizations(1, 1, 1, done)
 		})
 	})
 })
