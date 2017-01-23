0e0a90257c8dd1595990c97f564feff7a699293e
maxkoryukov <maxkoryukov@yandex.ru>
2017-01-23 05:09:53 +0500

    make eslint happy :lollipop:

--- test/resources/optimizations.spec.js
+++ test/resources/optimizations.spec.js
@@ -1,8 +1,8 @@
 "use strict"
 
-const _     = require("lodash")
+// const _     = require("lodash")
+// const sinon = require("sinon")
 
-const sinon = require("sinon")
 const request = require("superagent")
 const saMock  = require("superagent-mocker")(request)
 
@@ -15,32 +15,32 @@ describe("resources/optimizations.spec", () => {
 	describe("SDK methods", () => {
 		const route4me = new Route4Me(testApiKey)
 		const resource = new Optimizations(route4me)
-		let req = null
+		let req
 
 		beforeEach(() => {
 			req = null
-			saMock.get('*', (req) => req)
+			saMock.get("*", (r) => { req = r; return {} })
 		})
 
-		afterEach(()=>{
+		afterEach(() => {
 			saMock.clearRoutes()
 		})
 
-		describe("list", ()=> {
+		describe("list", () => {
 			it("should call route4me", (done) => {
-				resource.list([1,2,3], 100, null, (err, res) => {
-					//expect(requestStub.callCount).is.equal(1)
+				resource.list([1, 2, 3], 100, null, (err, res) => {
+					// expect(requestStub.callCount).is.equal(1)
 					expect(err).is.null
 					expect(res).is.not.null
 
-					expect(res).has.property("url")
+					expect(req).has.property("url")
 						.and.is.equal("https://route4me.com/api.v4/optimization_problem.php")
 
-					expect(res).has.property("body")
-					expect(res).has.property("query")
+					expect(req).has.property("body")
+					expect(req).has.property("query")
 
-					const q = res.query
-					const b = res.body
+					const q = req.query
+					const b = req.body
 
 					expect(q).has.property("states")
 						.and.is.equal("1,2,3")
@@ -49,8 +49,10 @@ describe("resources/optimizations.spec", () => {
 				})
 			})
 
-			it("should return error on invalid states", done => {
+			it("should return error on invalid states", (done) => {
 				resource.list(undefined, 100, null, (err, res) => {
+					expect(res).is.empty
+
 					expect(err).is.not.null
 					expect(err).is.instanceof(Error)
 					expect(err).has.property("message")
