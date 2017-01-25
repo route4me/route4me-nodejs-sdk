7fa0ac8516a05f677e27eae4a89d3b367f673b32
maxkoryukov <maxkoryukov@yandex.ru>
2017-01-26 04:08:27 +0500

    eslint rules for tests

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -9,5 +9,12 @@ exports = module.exports = {
 	"rules": {
 		"no-unused-expressions": "off",   // because of `expect(x).to.be.true`
 		"camelcase": "off",  // don't care...
+		"comma-dangle": ["error", {
+			"arrays": "only-multiline",
+			"objects": "only-multiline",
+			"imports": "only-multiline",
+			"exports": "only-multiline",
+			"functions": "ignore",
+		}]
 	}
 }
