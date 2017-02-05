3d45715e24b9df8834165ad22b7a627ee68d5656
maxkoryukov <maxkoryukov@yandex.ru>
2017-02-06 03:35:47 +0500

    DEV: addresses
    fix #6

--- src/resources/addresses.js
+++ src/resources/addresses.js
@@ -1,6 +1,15 @@
 "use strict"
 
-// const utils = require("../utils")
+const errors          = require("./../errors")
+
+function _markVisitedValidate(data) {
+	if (data && true === data.status) {
+		return true
+	}
+	return new errors.Route4MeValidationError("Invalid response", data)
+}
+
+const _markDepartedValidate = _markVisitedValidate
 
 /**
  * @namespace
@@ -23,11 +32,11 @@ class Addresses {
 	/**
 	 * Update custom data of the address (as a route destination).
 	 *
-	 * @see {@link https://route4me.io/docs/#update-a-route Route4Me API}
+	 * @see {@link https://route4me.io/docs/#update-a-route}
 	 * @category Addresses
 	 * @since 0.1.8
 	 *
-	 * @param {number} id       - Route destination (address) ID
+	 * @param {number} id       - Address ID
 	 * @param {string} routeId  - Route ID
 	 * @param {Object<string, string>} customFields - Any string dictionary
 	 * @param {module:route4me-node~RequestCallback<jsonschema:Addresses.Address>} [callback]
@@ -42,6 +51,132 @@ class Addresses {
 			},
 			body: customFields,
 			validationContext: "Addresses.Address",
+		}, callback)
+	}
+
+	/**
+	 * Mark as Detected as Departed
+	 *
+	 * The example refers to the process of marking an address as Detected as Departed.
+	 *
+	 * @see {@link https://route4me.io/docs/#mark-as-detected-as-departed}
+	 * @category Addresses
+	 * @since 0.1.9
+	 *
+	 * @param {number} id       - Address ID
+	 * @param {string} routeId  - Route ID
+	 * @param {boolean} value   - Actual value
+	 * @param {module:route4me-node~RequestCallback<Addresses.Address>} [callback]
+	 */
+	markDetectedDeparted(id, routeId, value, callback) {
+		const qs = {
+			"route_destination_id": id,
+			"route_id": routeId,
+		}
+
+		return this.r._makeRequest({
+			method: "PUT",
+			path: "/api.v4/address.php",
+			qs,
+			body: {
+				"is_departed": true === value,
+			},
+			validationContext: "Addresses.Address",
+		}, callback)
+	}
+
+	/**
+	 * Mark as Detected as Visited
+	 *
+	 * The example refers to the process of marking an address as Detected as Visited.
+	 *
+	 * @see {@link https://route4me.io/docs/#mark-as-detected-as-visited}
+	 * @category Addresses
+	 * @since 0.1.9
+	 *
+	 * @param {number} id       - Address ID
+	 * @param {string} routeId  - Route ID
+	 * @param {boolean} value   - Actual value
+	 * @param {module:route4me-node~RequestCallback<Addresses.Address>} [callback]
+	 */
+	markDetectedVisited(id, routeId, value, callback) {
+		const qs = {
+			"route_destination_id": id,
+			"route_id": routeId,
+		}
+
+		return this.r._makeRequest({
+			method: "PUT",
+			path: "/api.v4/address.php",
+			qs,
+			body: {
+				"is_visited": true === value,
+			},
+			validationContext: "Addresses.Address",
+		}, callback)
+	}
+
+	/**
+	 * Mark an address of a route as visited.
+	 *
+	 * @see {@link https://route4me.io/docs/#mark-as-visited}
+	 * @category Addresses
+	 * @since 0.1.9
+	 *
+	 * @todo HACK: method-conflict. API suggest to use "GET", SDK uses "PUT"
+	 *
+	 * @param {number} id       - Address ID
+	 * @param {string} routeId  - Route ID
+	 * @param {number} memberId - Member ID
+	 * @param {boolean} value   - Actual value
+	 * @param {module:route4me-node~RequestCallback<bool>} [callback]
+	 */
+	markVisited(id, routeId, memberId, value, callback) {
+		const qs = {
+			"address_id": id,
+			"route_id": routeId,
+			"member_id": memberId,
+		}
+
+		qs["is_visited"] = value ? "1" : "0"
+
+		return this.r._makeRequest({
+			method: "PUT",
+			path: "/actions/address/update_address_visited.php",
+			qs,
+			validationContext: _markVisitedValidate,
+		}, callback)
+	}
+
+	/**
+	 * Mark an address of a route as departed.
+	 *
+	 * @see {@link https://route4me.io/docs/#mark-as-departed}
+	 * @category Addresses
+	 * @since 0.1.9
+	 *
+	 * @todo HACK: method-conflict. API suggest to use "GET", SDK uses "PUT"
+	 *
+	 * @param {number} id       - Address ID
+	 * @param {string} routeId  - Route ID
+	 * @param {number} memberId - Member ID
+	 * @param {boolean} value   - Actual value
+	 * @param {module:route4me-node~RequestCallback<bool>} [callback]
+	 */
+	markDeparted(id, routeId, memberId, value, callback) {
+		const qs = {
+			"address_id": id,
+			"route_id": routeId,
+			"member_id": memberId,
+		}
+
+		qs["is_departed"] = value ? "1" : "0"
+
+		return this.r._makeRequest({
+			method: "PUT",
+			path: "/api/route/mark_address_departed.php",
+			qs,
+			validationContext: _markDepartedValidate,
 		}, callback)
 	}
 }
--- test/resources/addresses.spec.js
+++ test/resources/addresses.spec.js
@@ -22,7 +22,7 @@ describe(helper.toSuiteName(__filename), () => {
 			saMock.get("*", (r) =>  { req = r; req.method = "GET";    return {} })
 			saMock.post("*", (r) => { req = r; req.method = "POST";   return {} })
 			saMock.del("*", (r) =>  { req = r; req.method = "DELETE"; return {} })
-			saMock.put("*", (r) =>  { req = r; req.method = "PUT";    return {} })
+			// saMock.put("*", (r) =>  { req = r; req.method = "PUT";    return {} })
 		})
 
 		afterEach(() => {
@@ -30,6 +30,15 @@ describe(helper.toSuiteName(__filename), () => {
 		})
 
 		describe("updateCustomData", () => {
+			beforeEach(() => {
+				// TODO : mock in helper
+				saMock.put("*", (r) =>  { req = r; req.method = "PUT"; return {} })
+			})
+
+			afterEach(() => {
+				saMock.clearRoutes()
+			})
+
 			it("should call route4me", (done) => {
 				const customFields = { "banana": false }
 
@@ -40,6 +49,122 @@ describe(helper.toSuiteName(__filename), () => {
 						"route_id": "41",
 						"route_destination_id": "31" },
 						{ "banana": false }
+					)
+					done()
+				})
+			})
+		})
+
+		describe("markDetectedVisited", () => {
+			beforeEach(() => {
+				// TODO : mock in helper
+				saMock.put("*", (r) =>  { req = r; req.method = "PUT"; return {} })
+			})
+
+			afterEach(() => {
+				saMock.clearRoutes()
+			})
+
+			it("should call route4me", (done) => {
+				resource.markDetectedVisited(31, 41, true, (err, res) => {
+					expect(err).is.null
+					expect(res).is.not.null
+					helper.expectRequest(req,
+						"PUT", "https://route4me.com/api.v4/address.php",
+						{
+							"route_destination_id": "31",
+							"route_id": "41",
+						},
+						{
+							"is_visited": true
+						}
+					)
+					done()
+				})
+			})
+		})
+
+		describe("markDetectedDeparted", () => {
+			beforeEach(() => {
+				// TODO : mock in helper
+				saMock.put("*", (r) =>  { req = r; req.method = "PUT"; return {} })
+			})
+
+			afterEach(() => {
+				saMock.clearRoutes()
+			})
+
+			it("should call route4me", (done) => {
+				resource.markDetectedDeparted(12, 13, false, (err, res) => {
+					expect(err).is.null
+					expect(res).is.not.null
+					helper.expectRequest(req,
+						"PUT", "https://route4me.com/api.v4/address.php",
+						{
+							"route_destination_id": "12",
+							"route_id": "13",
+						},
+						{
+							"is_departed": false
+						}
+					)
+					done()
+				})
+			})
+		})
+
+		describe("markVisited", () => {
+			beforeEach(() => {
+				// TODO : mock in helper
+				saMock.put("*", (r) =>  { req = r; req.method = "PUT"; return { body: { status: true } } })
+			})
+
+			afterEach(() => {
+				saMock.clearRoutes()
+			})
+
+			it("should call route4me", (done) => {
+				resource.markVisited(31, 41, 51, true, (err, res) => {
+					expect(err).is.null
+					expect(res).is.not.null
+					helper.expectRequest(req,
+						"PUT", "https://route4me.com/actions/address/update_address_visited.php",
+						{
+							"address_id": "31",
+							"route_id": "41",
+							"member_id": "51",
+							"is_visited": "1",
+						},
+						null
+					)
+					done()
+				})
+			})
+		})
+
+		describe("markDeparted", () => {
+			beforeEach(() => {
+				// TODO : mock in helper
+				saMock.put("*", (r) =>  { req = r; req.method = "PUT"; return { body: { status: true } } })
+			})
+
+			afterEach(() => {
+				saMock.clearRoutes()
+			})
+
+			it("should call route4me", (done) => {
+				resource.markDeparted(12, 13, 14, false, (err, res) => {
+					expect(err).is.null
+					expect(res).is.not.null
+					helper.expectRequest(req,
+						"PUT", "https://route4me.com/api/route/mark_address_departed.php",
+						{
+							"address_id": "12",
+							"route_id": "13",
+							"member_id": "14",
+							"is_departed": "0"
+						},
+						null
 					)
 					done()
 				})
